fb1d6fb26a2125c6bac653946c6d94889290c13d
Nallamala sai bharadwaja <nallamalasaibharadwaja@gmail.com>
2026-01-12 22:40:55 +0530

    Add files via upload

---
+++ Temp_in_celsius_convert_into_farhenheit.py
@@ -0,0 +1,4 @@
+#write a python program to enter temperature in celsius and convert it into farhenheit
+celsius=float(input("Enter temp in celsius:"))
+farheniheit = (celsius*9/5)+32
+print("Temperature in farenheit:",farheniheit)
