1afb606e4a95aef5a15337d806c1292bb852d83b
Nallamala sai Bharadwaja <nallamalasaibharadwaja@gmail.com>
2026-01-22 21:38:53 +0530

    Add files via upload

---
+++ Basic_functions.py
@@ -0,0 +1,17 @@
+'''#Write a program to add two numbers and return the result
+def sum(a,b):
+    sum=a+b
+    return(sum)
+result=(sum(10,20))
+print(result)
+'''
+#Write a function to check wheather a number is even or odd
+def even_odd(num):
+    if num%2==0:
+        return "Even"
+    else:
+        return "odd"
+result=(even_odd(10))
+print(result)
+
+ 
