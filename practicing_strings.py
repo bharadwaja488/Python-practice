da8314a3f09df75d512735d271bf20c6008d5244
bharadwaja488 <nallamalasaibharadwaja@gmail.com>
2026-05-21 20:49:03 +0530

    Update practicing_strings script

--- practicing_strings.py
+++ practicing_strings.py
@@ -1,2 +1,36 @@
+# Reversing a string 
 name = "Python"
 print(name[::-1])
+
+#length of string
+name=input("Enter name: ")
+print(len(name))
+
+#upper case
+name = "python"
+print(name.upper())
+
+#lower case 
+word = input("Enter word: ")
+print(word.lower())
+
+#Replacing a word 
+sentence = "I love java"
+print(sentence.replace("java","Python"))
+
+#split string
+email = "sai@gmail.com"
+print(email.split("."))
+
+#Removing extra spaces
+name="     python    "
+print(name.strip())
+
+#palindrome checker
+word = input("Enter word: ")
+if word == word[::-1]:
+    print("Palindrome")
+else:
+    print("Not palindrome")
+    
+
